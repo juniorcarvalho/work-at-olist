fc59143c8d295da50aa42b312aa28ce1202ec890
Júnior Carvalho <joseadolfojr@gmail.com>
2020-05-19 19:40:11 -0300

    configure dj-static

--- api_book/settings.py
+++ api_book/settings.py
@@ -114,6 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
